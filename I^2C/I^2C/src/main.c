98ccd9eb71c96c59b8ca69201d470ccb41574fd2
Moritz Zeitler <moritz.zeitler@web.de>
2021-05-05 15:50:18 +0200

    added running state byte write read, and schematics

--- I^2C/I^2C/src/main.c
+++ I^2C/I^2C/src/main.c
@@ -28,21 +28,22 @@
 /*
 * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
 */
+
 #include <asf.h>
 #include <avr/io.h>
 #include "Usart.h"
 #include <avr/pgmspace.h>
-#define EEDEVADR 0b10100000
 
-#define I2C_SCK 100
+#define SLA_W 0b10100000
+#define SLA_R 0b10100001
+
 #define F_CPU 8000000UL
 
 void TWIInit(void)
 {
-	//set SCL to 400kHz
 	TWSR = 0x00;
-	TWBR = 0x0C;
-	TWBR = F_CPU / I2C_SCK - 16;
+	//TWBR = 0x0C;
+	TWBR = 0x0C; // 400 kHz
 	//enable TWI
 	TWCR = (1<<TWEN);
 }
@@ -58,21 +59,20 @@ void TWIStop(void)
 	TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
 }
 
-/*
+/**
 char i2c_slaw_tx(void) {
 TWDR = SLA_W; //CHIP_ADDR //Load SLA_W into TWDR Register.
 TWCR = (1<<TWINT) | (1<<TWEN); //Clear TWINT bit in TWCR to start transmission
 while (!(TWCR & (1<<TWINT))) //of address
 ;
 return ((TWSR & 0xF8) == 0x18); // == MT_SLA_ACK, SLA+W has been transmitted; ACK received
-}
-*/
+}*/
+
 void TWIWrite(uint8_t u8data)
 {
 	TWDR = u8data;
 	TWCR = (1<<TWINT)|(1<<TWEN);
 	while ((TWCR & (1<<TWINT)) == 0);
-
 }
 
 uint8_t TWIReadACK(void)
@@ -97,60 +97,73 @@ uint8_t TWIGetStatus(void)
 	return status;
 }
 
-uint8_t EEWriteByte(uint16_t u16addr, uint8_t u8data)
+uint8_t EEWriteByte(uint8_t u8addr, uint8_t u8data)
 {
 	TWIStart();
-	if (TWIGetStatus() != 0x08)
-	printf("error \r\n");
+	if (TWIGetStatus() != 0x08){
+		printf("error \r\n");
+	}
 	//select devise and send A2 A1 A0 address bits
-	TWIWrite((EEDEVADR)|(uint8_t)((u16addr & 0x0700)>>7));
-	if (TWIGetStatus() != 0x18)
-	printf("error \r\n");;
+	TWIWrite(SLA_W);
+	if (TWIGetStatus() != 0x18){
+		printf("error \r\n");
+	}
 	//send the rest of address
-	TWIWrite((uint8_t)(u16addr));
-	if (TWIGetStatus() != 0x28)
-	printf("error \r\n");
+	TWIWrite((uint8_t)(u8addr));
+	if (TWIGetStatus() != 0x28){
+		printf("error \r\n");
+	}
 	//write byte to eeprom
 	TWIWrite(u8data);
-	if (TWIGetStatus() != 0x28)
-	printf("error \r\n");
+	if (TWIGetStatus() != 0x28){
+		printf("error \r\n");
+	}
 	TWIStop();
 	printf("success \r\n");
 }
 
 
-uint8_t EEReadByte(uint16_t u16addr, uint8_t *u8data)
+uint8_t EEReadByte(uint8_t u8addr, uint8_t *u8data)
 {
 	//uint8_t databyte;
 	TWIStart();
 	if (TWIGetStatus() != 0x08){
-		uint8_t EEReadByte(uint16_t u16addr, uint8_t *u8data);
+		printf("error \r\n");
 	}
-	//uint8_t databyte;
-	TWIStart();
-	if (TWIGetStatus() != 0x08)
-	printf("error \r\n");
 	//select devise and send A2 A1 A0 address bits
-	TWIWrite((EEDEVADR)|((uint8_t)((u16addr & 0x0700)>>7)));
-	if (TWIGetStatus() != 0x18)
-	printf("error \r\n");
+	TWIWrite(SLA_W);
+	if (TWIGetStatus() != 0x18){
+		printf("error \r\n");
+	}
 	//send the rest of address
-	TWIWrite((uint8_t)(u16addr));
-	if (TWIGetStatus() != 0x28)
-	printf("error \r\n");
+	TWIWrite((uint8_t)(u8addr));
+	if (TWIGetStatus() != 0x28){
+		printf("error \r\n");
+	}
 	//send start
 	TWIStart();
-	if (TWIGetStatus() != 0x10)
-	printf("error \r\n");
+	if (TWIGetStatus() != 0x10){
+		printf("error \r\n");
+	}
 	//select devise and send read bit
-	TWIWrite((EEDEVADR)|((uint8_t)((u16addr & 0x0700)>>7))|1);
-	if (TWIGetStatus() != 0x40)
-	printf("error \r\n");
+	TWIWrite(SLA_R);
+	if (TWIGetStatus() != 0x40){
+		printf("error \r\n");
+	}
 	*u8data = TWIReadNACK();
-	if (TWIGetStatus() != 0x58)
-	printf("error \r\n");
+	if (TWIGetStatus() != 0x58){
+		printf("error \r\n");
+	}
 	TWIStop();
 	printf("success \r\n");
+}
+
+char i2c_slaw_tx(uint8_t address) {
+	TWDR = address; //CHIP_ADDR //Load SLA_W into TWDR Register.
+	TWCR = (1<<TWINT) | (1<<TWEN); //Clear TWINT bit in TWCR to start transmission
+	while (!(TWCR & (1<<TWINT))) //of address
+	;
+	return ((TWSR & 0xF8) == 0x18); // == MT_SLA_ACK, SLA+W has been transmitted; ACK received
 }
 
 int main (void)
@@ -160,19 +173,28 @@ int main (void)
 	
 	uint8_t u8ebyte;
 	uint8_t u8erbyte;
-	uint16_t u16eaddress = 0x07F0;
+	uint8_t u8eaddress = 0x5E;
 	uint8_t page = 5;
 	uint8_t i;
 	
 	DDRB = 0xFF;
 	PORTB = 0x00;
-	
+	printf("Starting \r\n");
 	TWIInit();
+	//TWIStart();
+	//printf("%#04x \r\n",TWIGetStatus());
 	//assign our stream to standard I/O streams
-	printf("\nWrite byte %#04x to eeprom address %#04x", 0x58, u16eaddress);
-	EEWriteByte(u16eaddress, 0x58);
-	printf("\nRead byte From eeprom");
-	EEReadByte(u16eaddress, &u8ebyte);
-	printf("\n*%#04x = %#04x", u16eaddress, u8ebyte);
+	//select devise and send A2 A1 A0 address bits
+	//TWIWrite(SLA_W);
+	//printf("%#04x \r\n",TWIGetStatus());
+	//TWIReadACK();
+	//printf("%#04x \r\n",TWIGetStatus());
+	
+	
+	printf("\nWrite byte %#04x to eeprom address %#04x \r\n", 0x58, u8eaddress);
+	EEWriteByte(u8eaddress, 0x58);
+	printf("\nRead byte From eeprom \r\n");
+	EEReadByte(u8eaddress, &u8ebyte);
+	printf("\n*%#04x = %#04x", u8eaddress, u8ebyte);
 
 }
