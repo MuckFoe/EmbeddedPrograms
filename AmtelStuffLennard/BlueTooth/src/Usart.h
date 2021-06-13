778867dfd8f11becc7a76d394032307d47ad9b9b
LennardRose <lennard.rose@student.fhws.de>
2021-06-13 17:00:33 +0200

    Bluetooth stuff first steps, no time to test but seems valid, maybe too easy but you never know

---
+++ AmtelStuffLennard/BlueTooth/src/Usart.h
@@ -0,0 +1,29 @@
+#ifndef USART_H_
+#define USART_H_
+
+#include <stdio.h>
+
+#ifndef F_CPU
+	#define F_CPU 8000000UL
+#endif
+#define BAUD		9600UL
+#define UBBR_BAUD	(F_CPU/(BAUD*16)-1)
+
+
+void USART_Init( void );
+
+void USART_Transmit( unsigned char data );
+
+int usart_transmit_printf( char var, FILE *stream );
+
+uint8_t USART_Receive( void );
+
+void usart_putchar(unsigned char data);
+
+int usart_putchar_printf(char , FILE *);
+
+void usart_setup_stdio_stream( void );
+
+void USART_putstring(char* StringPtr);
+
+#endif
